5e9b0bb301540e28743c7b52d529ca2702cdfe03
Jeehun Kim (김지훈) <jhkim3@4dreplay.com>
2024-07-23 13:16:08 +0900

    aws service 추가

--- src/aws/aws.module.ts
+++ src/aws/aws.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MulterconfigService } from './MulterConfigService';
+// import { MulterconfigService } from './MulterConfigService';
 import { AwsController } from './aws.controller';
 import { AwsService } from './aws.service';
+// import { MulterBuilder } from './multerBuilder';
+// import { CreateBodyAllMulterOptions } from './multerOptions';
+import { multerOptionsFactory } from './multerOptionFactory';
 
 @Module({
   imports: [
     MulterModule.registerAsync({
       imports: [ConfigModule],
-      useFactory: MulterconfigService,
+      useFactory: multerOptionsFactory,
       inject: [ConfigService],
     }),
   ],
--- src/aws/aws.service.ts
+++ src/aws/aws.service.ts
@@ -1,5 +1,5 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { MulterconfigService } from './MulterConfigService';
+// import { MulterconfigService } from './MulterConfigService.ts_';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
@@ -13,25 +13,25 @@ export class AwsService {
     return files;
   }
 
-  fileDelete(file: string) {
-    const multerConfig = MulterconfigService(this.configService);
-    const s3 = multerConfig.storage.s3;
-    const Bucket = multerConfig.storage.bucket;
-    const parmas = {
-      Bucket,
-      key: file,
-    };
+  // fileDelete(file: string) {
+  //   const multerConfig = MulterconfigService(this.configService);
+  //   const s3 = multerConfig.storage.s3;
+  //   const Bucket = multerConfig.storage.bucket;
+  //   const parmas = {
+  //     Bucket,
+  //     key: file,
+  //   };
 
-    try {
-      s3.deleteObject(parmas, function (err: any, data: any) {
-        if (err) {
-          throw new BadRequestException();
-        } else {
-          return data;
-        }
-      });
-    } catch (e) {
-      throw new BadRequestException();
-    }
-  }
+  //   try {
+  //     s3.deleteObject(parmas, function (err: any, data: any) {
+  //       if (err) {
+  //         throw new BadRequestException();
+  //       } else {
+  //         return data;
+  //       }
+  //     });
+  //   } catch (e) {
+  //     throw new BadRequestException();
+  //   }
+  // }
 }
